84ef543d1548f17e5f791fae8eb1464df9c3f462
clmarcl <marcel.bucher0203@gmail.com>
2026-07-15 09:52:39 +0200

    neu

--- einfache Erklärungen/Funktionen.py
+++ einfache Erklärungen/Funktionen.py
@@ -5,3 +5,20 @@ def greeting():
     print("Viel Spaß beim Programmieren")
 
 greeting()
+
+def trinken(menge):
+    a = 0
+    while a < menge:
+        print("trinken")
+        a += 1
+
+trinken(int(input("Menge eingeben: ")))
+print(trinken(int(input("Menge eingeben: "))))
+
+def maximum(a, b):
+    if a<b:
+        return b
+    else:
+        return a
+
+print(type(maximum(5, 6)))
