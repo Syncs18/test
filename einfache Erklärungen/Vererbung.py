a52106b62834d9c45474327abebca3efa1705aff
clmarcl <marcel.bucher0203@gmail.com>
2026-07-16 13:13:59 +0200

    neu

--- einfache Erklärungen/Vererbung.py
+++ einfache Erklärungen/Vererbung.py
@@ -9,6 +9,9 @@ class Animal:
     def sleep(self):
         print("Sleeping")
 
+    def move_fast(self):
+        print("Moving: 20km/h")
+
 class Dog(Animal):
 
     def bark(self):
@@ -19,9 +22,30 @@ class Cat(Animal):
     def purr(self):
         print("schnurr")
 
+class Tiger(Animal):
+    def move_fast(self):
+        print("Moving: 80km/h")
+
+class Owl(Animal):
+
+
+    def __init__(self, name, age, color, fav_food, hunting_instinct):
+        super().__init__(name, age, color, fav_food)
+        self.hunting_instinct = hunting_instinct
+
+
+    def sleep(self):
+        super().sleep()
+        print("Gleichgewicht halten")
+
 dog = Dog("Dog", 10, "red", "Fleisch")
 dog.sleep()
 cat = Cat("Cat", 20, "blue", "Fisch")
 print(cat.age)
 
+tiger = Tiger("Tiger", 20, "red", "Fleisch")
+dog.move_fast()
+tiger.move_fast()
+owl = Owl("Eule", 20, "red", "Wurm",7)
+owl.sleep()
 
